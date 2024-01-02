8ece7189a859f9b9649ffef9f38ba66220a7709d
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2024-01-02 13:40:46 -0300

    Add files via upload

---
+++ NetworkApp/NetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Lucas Neves dos santos pompeu on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
