9fcdff34e6e21f4021790ecc6a4627246b773a0c
Lucas Neves <lucas.neves12@icloud.com>
2024-01-03 09:42:16 -0300

    Configuring the app

---
+++ NetworkApp/NetworkApp/Home/Service/HomeService.swift
@@ -0,0 +1,13 @@
+//
+//  HomeService.swift
+//  NetworkApp
+//
+//  Created by Lucas Neves dos santos pompeu on 02/01/24.
+//
+
+import Foundation
+
+class HomeService {
+
+    
+}
---
+++ NetworkApp/NetworkApp/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  NetworkApp
+//
+//  Created by Lucas Neves dos santos pompeu on 02/01/24.
+//
+
+import Foundation
+
+class HomeViewModel {
+
+    public func fetchRequest() {
+        
+    }
+}
